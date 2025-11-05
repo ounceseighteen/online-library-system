7a64474a0ba72a4eb6e15d518c4d29b0728ee6d9
Вершинин Н. А. <student@edu.ru>
2025-11-06 00:21:02 +0400

    Добавлен метод получения статуса книги

--- app/book_service.py
+++ app/book_service.py
@@ -4,3 +4,6 @@ class BookService:
 
     def get_book_details(self, book_id):
         return f"Информация о книге {book_id}"
+
+    def get_book_status(self, book_id):
+        return f"Статус книги {book_id}"
