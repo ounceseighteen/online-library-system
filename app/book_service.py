dc7b712c54a44d4edddef0c8e8220a84b88839fc
Вершинин Н. А. <student@edu.ru>
2025-11-06 00:18:41 +0400

    Добавлена аутентификация в BookService

--- app/book_service.py
+++ app/book_service.py
@@ -4,3 +4,6 @@ class BookService:
 
     def get_book_details(self, book_id):
         return f"Информация о книге {book_id}"
+
+    def authenticate_user(self, username, password):
+        return f"Аутентификация пользователя {username}"
